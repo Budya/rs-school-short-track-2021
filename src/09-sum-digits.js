d75d76ea00d7823c355a520fd85ffc40370fa6aa
Dima <budya2012@gmail.com>
2021-04-13 00:48:13 +0300

    feat: implement sum of digits function

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,18 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const radix = 10;
+  let result = 0;
+  const stringArr = n.toString().split('');
+  const numArr = stringArr.map((item) => parseInt(item, radix));
+  for (let i = 0; i < numArr.length; i++) {
+    result += numArr[i];
+  }
+  if (result < 10) {
+    return result;
+  }
+  return getSumOfDigits(result);
 }
 
 module.exports = getSumOfDigits;
